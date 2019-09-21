b072938834af3897e65cae21bec72e6bb54875e7
KidpaddleetcieGlin <37848836+Kidpaddleetcie@users.noreply.github.com>
2019-09-22 01:51:03 +0200

    Add files via upload

---
+++ assets/js/generator.js
@@ -0,0 +1,15 @@
+let gen = " ";
+let visu_table = ["Pas d'incrustation", "Très peu d'incrustation", "Beaucoup d'incrustation", "Effets Bordéliques"];
+let sm_table = ["Aucun Sentence-Mixing", "Très peu de Sentence-Mixing", "Beaucoup de Sentence-Mixing"];
+
+
+for (let ii = 0; ii < poopeurs.length; ii++) {
+    YTPmakers = poopeurs[ii];
+    gen += '<a href="' + YTPmakers.url + '"><div class="boite">';
+    gen += '<h1 class="nom">' + YTPmakers.Nom + '</h1>'
+    gen += '<img class="pp" src="' + YTPmakers.img + '"/>'
+    gen += '<p class="visu"> ' + visu_table[YTPmakers.Visu - 1] + '</p>'
+    gen += '<p class="sm"> ' + sm_table[YTPmakers.SM - 1] + '</p>'
+    gen += '</div></a>';
+}
+document.getElementById("poopeurs").innerHTML = gen;
---
+++ assets/js/poopeurs.js
@@ -0,0 +1,46 @@
+let poopeurs = [{
+        "Nom": "Diddy64Wii",
+        "Visu": 4,
+        "SM": 3,
+        "img": "https://yt3.ggpht.com/a/AGF-l7-qEaOqNTiKTEMyOGdeue3ZIgOiRI4sEkNtzg=s288-c-k-c0xffffffff-no-rj-mo",
+        "url": "https://www.youtube.com/channel/UCcvL14enJ8bkf1fxofPamyg"
+    },
+    {
+        "Nom": "MrMental4",
+        "Visu": 3,
+        "SM": 2,
+        "img": "https://yt3.ggpht.com/a/AGF-l7-0Dx3cG0ZtvypPSGDEGBlZ1Y6yx83c_7Lhrg=s288-c-k-c0xffffffff-no-rj-mo",
+        "url": "https://www.youtube.com/user/MrMental4/videos"
+    },
+    {
+        "Nom": "Paprika",
+        "Visu": 2,
+        "SM": 1,
+        "img": "https://yt3.ggpht.com/a/AGF-l7-MklGy5eCI_JYcn0sySnUtggB7mstv3i6LIw=s176-c-k-c0x00ffffff-no-rj-mo",
+        "url": "https://www.youtube.com/channel/UC56vGrQroQ4WDyBhBHpNuYg"
+    },
+    {
+        "Nom": "Fynn Tavvar",
+        "Visu": 3,
+        "SM": 3,
+        "img": "https://yt3.ggpht.com/a/AGF-l79LHzU_2PkYU-jIr-xwCcDQ7FbaJ6NSSij-_Q=s288-c-k-c0xffffffff-no-rj-mo",
+        "url": "https://www.youtube.com/user/FynnTavvar"
+    },
+    {
+        "Nom": "Jefaischierlesgens",
+        "Visu": 4,
+        "SM": 2,
+        "img": "https://yt3.ggpht.com/a/AGF-l7_siBhFeWtDWY6-9uwUft_I0Ss9JDixBl3Pag=s288-c-k-c0xffffffff-no-rj-mo",
+        "url": "https://www.youtube.com/user/Jefaischierlesgens/videos"
+    },
+    {
+        "Nom": "Poopulaire",
+        "Visu": 4,
+        "SM": 3,
+        "img": "https://yt3.ggpht.com/a/AGF-l79floUPLYhXU1pbFLVV40cZctYj4xuibQTZQA=s288-c-k-c0xffffffff-no-rj-mo",
+        "url": "https://www.youtube.com/channel/UCbK5TxzVZrgbcDUT_XsYgVg"
+    }
+
+
+
+]
