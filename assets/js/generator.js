5527b3dbb888462661dbb3f647f67c7590db2abc
Kidpaddleetcie <37848836+Kidpaddleetcie@users.noreply.github.com>
2021-05-02 19:24:14 +0200

    rectification

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -4,12 +4,13 @@ let sm_table = ["Aucun Sentence-Mixing", "Très peu de Sentence-Mixing", "Beauco
 
 for (let ii = 0; ii < poopeurs.length; ii++) {
     YTPmakers = poopeurs[ii];
+    idytp = YTPmakers.url
     gen += '<a href="' + YTPmakers.url + '"><div class="boite">';
     gen += '<h1 class="nom">' + YTPmakers.Nom + '</h1>'
     gen += '<img class="pp" src="' + YTPmakers.img + '"/>'
     gen += '<p class="visu"> ' + visu_table[YTPmakers.Visu - 1] + '</p>'
     gen += '<p class="sm"> ' + sm_table[YTPmakers.SM - 1] + '</p>'
-    gen += '<div class="g-ytsubscribe" data-channel="' + [YTPmakers.url].split('/')[3] + '" data-layout="default" data-theme="dark" data-count="default"></div>'
+    gen += '<div class="g-ytsubscribe" data-channel="' + idytp.split('/')[3] + '" data-layout="default" data-theme="dark" data-count="default"></div>'
     gen += '</div></a>';
 }
 document.getElementById("poopeurs").innerHTML = gen;
