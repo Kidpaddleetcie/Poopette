674ecf1fb8d5a8fab744a50d7f160575773b2e6f
Kidpaddleetcie <37848836+Kidpaddleetcie@users.noreply.github.com>
2021-05-02 22:01:03 +0200

    Update generator.js

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -4,13 +4,20 @@ let sm_table = ["Aucun Sentence-Mixing", "Très peu de Sentence-Mixing", "Beauco
 
 for (let ii = 0; ii < poopeurs.length; ii++) {
     YTPmakers = poopeurs[ii];
+    idytp = YTPmakers.url
     gen += '<a href="' + YTPmakers.url + '"><div class="boite">';
     gen += '<h1 class="nom">' + YTPmakers.Nom + '</h1>'
     gen += '<img class="pp" src="' + YTPmakers.img + '"/>'
     gen += '<p class="visu"> ' + visu_table[YTPmakers.Visu - 1] + '</p>'
     gen += '<p class="sm"> ' + sm_table[YTPmakers.SM - 1] + '</p>'
-    gen += '<div class="g-ytsubscribe" data-channel="'
-    gen += YTPmakers.id
+    gen += '<div class="g-ytsubscribe" '
+    if (idytp.split('/')[3] != "channel") {
+        gen += 'data-channel="' + idytp.split('/')[4];
+    } else {
+        gen += 'data-channelid="' + idytp.split('/')[4];
+    }
+
+
     gen += '" data-layout="default" data-theme="dark" data-count="default"></div>'
     gen += '</div></a>';
 }
